9e4dd23ebc7659ffb1b8a96c8c0cf58f3d16741e
germankochnev <kochgerm@gmail.com>
2025-02-20 20:55:13 +0300

    autodetect device

--- train_gpt2.py
+++ train_gpt2.py
@@ -6,6 +6,16 @@ import math
 
 from torch.nn import functional as F
 
+def get_device():
+    if torch.cuda.is_available():
+        return 'cuda'
+    elif hasattr(torch, 'mps') and torch.mps.is_available():
+        return 'mps'
+    else:
+        return 'cpu'
+
+device = get_device()
+
 @dataclass
 class GPTConfig:
     block_size: int = 1024
@@ -162,34 +172,34 @@ class GPT(nn.Module):
 
         return model
     
-# num_sequences = 5
-# max_length = 30
+num_sequences = 5
+max_length = 30
 
-# model = GPT.from_pretrained('gpt2')
-# model.eval()
-# model.to("cpu")
+model = GPT.from_pretrained('gpt2')
+model.eval()
+model.to(device)
 
-# import tiktoken
-# enc = tiktoken.get_encoding('gpt2')
-# tokens = enc.encode("Hello, I'm language model,")
-# tokens = torch.tensor(tokens, dtype=torch.long)
-# tokens = tokens.unsqueeze(0).repeat(num_sequences, 1)
-# x = tokens.to("cpu")
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_sequences, 1)
+x = tokens.to(device)
 
-# torch.manual_seed(10)
-# torch.mps.manual_seed(10)
+torch.manual_seed(10)
+torch.mps.manual_seed(10)
 
-# while x.size(1) < max_length:
-#     with torch.no_grad():
-#         logits = model(x)
-#         logits = logits[:, -1, :]
-#         probs = F.softmax(logits, dim=1)
-#         topk_probs, topk_indicies = torch.topk(probs, 50, dim=-1)
-#         ix = torch.multinomial(topk_probs, 1)
-#         xcol = torch.gather(topk_indicies, -1, ix)
-#         x = torch.cat((x, xcol), dim=1)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=1)
+        topk_probs, topk_indicies = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indicies, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
         
-# for i in range(num_sequences):
-#     tokens = x[i, :max_length].tolist()
-#     decoded = enc.decode(tokens=tokens)
-#     print(f"> {decoded}")
+for i in range(num_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens=tokens)
+    print(f"> {decoded}")
